303a0967ad005d2b95fb248008059f2a7b3b015b
Cgcraigon <c.g.craigon@dundee.ac.uk>
2015-02-24 15:31:15 +0000

    Update test1.py

--- public_html/cgi-bin/test1.py
+++ public_html/cgi-bin/test1.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
-chmod +x test1.py
-
 import cgi
 import cgitb
 import os
 from mod_python import apache 
-module = apache.import_module('models.py', path =[directory])
-
+dirctory = os.path.dirname(__models.py__)
+module = apache.import_module('models.py', path=[directory])
 cgitb.enable()
-
-form = cgi.FieldStorage()
-
-Gene = models.Gene([query])
 print "Content-Type: text/html"     # HTML is following
 print                               # blank line, end of headers
 print "<html><head><TITLE>CGI script output</TITLE></head>"
 print "<body><H1>Form values</H1>"
 print "<table><tr><th>Key</th><th>Value</th></tr>"
+form = cgi.FieldStorage()
+db=models.DBHandler()
+cursor=db.cursor()
+cursor.execute('select id_ref from probe where gene_is=%s',(form['query'],))
 
 for k in form.keys():
         print "<tr><td>%s</td><td>%s</td></tr>"%(k, form[k].value)
