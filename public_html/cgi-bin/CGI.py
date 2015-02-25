28bb088a34e5b7ebd8a493fd66c731d6e88e1574
Cgcraigon <c.g.craigon@dundee.ac.uk>
2015-02-25 10:12:24 +0000

    Update CGI.py

--- public_html/cgi-bin/CGI.py
+++ public_html/cgi-bin/CGI.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
+chmod +x CGI.py
+#Imports cgi modules 
 import cgi
 import cgitb
+#Imports os module required for use with apache. 
 import os
+#imports apache 
 from mod_python import apache 
+# links the CGI.py with the models.py for importing information the website. 
 dirctory = os.path.dirname(__models.py__)
 module = apache.import_module('models.py', path=[directory])
 cgitb.enable()
+form = cgi.FeildStorage()
+#creates a form where the output can be stored.
 print "Content-Type: text/html"     # HTML is following
 print                               # blank line, end of headers
 print "<html><head><TITLE>CGI script output</TITLE></head>"
 print "<body><H1>Form values</H1>"
 print "<table><tr><th>Key</th><th>Value</th></tr>"
-form = cgi.FieldStorage()
-db=models.DBHandler()
-cursor=db.cursor()
-cursor.execute('select id_ref from probe where gene_is=%s',(form['query'],))
-
 for k in form.keys():
         print "<tr><td>%s</td><td>%s</td></tr>"%(k, form[k].value)
-
+# Prints the table
 print "<table>"
 
 print "</body></html>"
