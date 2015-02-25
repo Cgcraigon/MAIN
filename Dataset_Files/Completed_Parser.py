617e1c730e9d6010db6315b4861480659a849e36
Cgcraigon <c.g.craigon@dundee.ac.uk>
2015-02-25 09:50:58 +0000

    Updated final

--- Dataset_Files/Completed_Parser.py
+++ Dataset_Files/Completed_Parser.py
@@ -1,57 +1,39 @@
 # -*- coding: iso-8859-1 -*-
 #takes the raw data file and creates 4 text files that can each be loaded into a table in mySQL
 #!/usr/bin/python
-
-# step 1. open the data file
+# -*- coding: iso-8859-1 -*-
+#opens the script dataset 'GDS2842_full.soft'
 infile= 'GDS3842_full.soft'
 fh= open(infile)
 line= fh.readline()
-
-#step 2. read the first line and then read more lines while the line doesn't match a specific pattern
- #need to complete th#step
-
-
+# read through the table until it identifies the column pattern.
 while line[:20] != '!dataset_table_begin':
     line=fh.readline()
-
 header= fh.readline().strip()
-
-#capture the column names
+#Defines the titles for each column 
 colnames={}
+#laocates column names
 index=0
 for title in header.split('\t'):
     colnames[title]=index
     print '%s\t%s'%(title,index)
     index=index+1
 #open our output files, one per table.
-
-#defines which columns are to go in each output file. For samples it is the 3rd header until the gene title header and they will be separated by '\t'
 genefile=open('genes.txt', 'w')
 expressionfile=open('expression.txt','w')
 probefile=open('probes.txt', 'w')
-
+#Assigns the feild names that correspond table columns
 genefields=['Gene ID', 'Gene symbol', 'Gene title']
 samples=header.split('\t')[2:int(colnames['Gene title'])]
 probefields=['ID_REF','Gene ID']
-
+# creates a row with the column headers 
 def buildrow(row, fields):
-    '''Creates a tab separated list of values according to the columns listed in fieldsrow: a list of values
-	fields: a list of columns. Only the values in row corresponding to the columns in fields are output
-	returns: A string that is a tab separated list of values terminated with a newline
-	'''
     newrow=[]
     for f in fields:
         newrow.append(row[int(colnames[f])])
     return "\t".join(newrow)+"\n"
-
-
-	#creates the rows for the expression file, is slightly different because for each probe and experiment there are several gene expression values.
+    #adds the sample values of expression data to the output files.
 def build_expression(row, samples):
-    '''Builds tab separated rows for expression data. For each of the samples listed 
-	it generates a line with the probe id, sample id and expression value.
-	row: a list of values
-	samples: a list of column headings corresponding to the samples
-	'''
     exprrows=[]
     for s in samples:
         newrow=[s,]
@@ -59,10 +41,9 @@ def build_expression(row, samples):
 	newrow.append(row[int(colnames[s])])
 	exprrows.append("\t".join(newrow))
     return "\n".join(exprrows)+"\n"
-	
-	
 #initialise a counter to count how many probe rows were processed.    
 #writes the data to the files 
+# also has an exception to help identify bugs with the code.  
 rows=0
 for line in fh.readlines():
     try:
@@ -75,14 +56,14 @@ for line in fh.readlines():
         rows=rows+1
     except:
 		pass
+#Closes the output files. 
 genefile.close()
 probefile.close()
 expressionfile.close()
+# print message informing that user of the progress and evident success of the script. 
 print '%s rows processed'%rows
 
-#close the created files after the data has been writen to them
 
-#print out a message to indicate success with outputting the data.
 
 
 
